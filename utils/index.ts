caad3223314d80db3301dca979815e52666a633d
Yueling Liu <stellaliuca@gmail.com>
2024-04-30 13:03:22 -0700

    updated search params

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,15 @@
-export async function fetchCars() {
+import { manufacturer } from '@constants';
+import { CarProps, FilterProps } from '@/types';
+
+export async function fetchCars(fileters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = fileters;
   const headers = {
     'X-RapidAPI-Key': '895f20bb7dmsh914c3d60fd5eb25p1ee91bjsnd14fd948399d',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
   const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
@@ -29,4 +33,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split('')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
